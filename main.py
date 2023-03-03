c535d498f005590db1d07ecaae56b40f782a17ec
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-03-02 22:00:17 -0500

    passcode

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from lib2to3.pytree import _Results
+
 import subprocess
 
-data = subprocess.check_output(['netsh','wlan','show','profiles' ]).decode('ut-f8',errors='backsplasreplace').split('\n')
+data = subprocess.check_output(['netsh','wlan','show','profiles' ]).decode('utf-8',errors='backsplasreplace').split('\n')
 
 profiles = [i.split(":")[1][1:-1]
             for i in data 
             if"All User Profiles" in i]
 
 for i in profiles:
+    
     try:
-        result = subprocess.check_output(['netsh','wlan','show','profile', i, 'key=clear' ]).decode('ut-f8',errors='backsplasreplace').split('\n')
-    result = [b.split(":")[1][1:-1] for b in 
-                _Results if "Key Content" in b]
-    try:
+        result = subprocess.check_output(['netsh','wlan','show','profile', i, 'key=clear' ]).decode('utf-8',errors='backsplasreplace').split('\n')
+        result = [b.split(":")[1][1:-1] for b in result if "Key Content" in b]
+   
         print("{:<30}|{:<}".format(i, result[0]))
-        expect IndexError:
+    except IndexError:
             print("{:<30}| {:<}".format(i, ""))
+    except subprocess.CalledProcessError:
+        print("{:<30}| {:<}".format(i, 'ENCODING ERROR'))
+        
+        input("")
